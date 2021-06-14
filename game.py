264acf6544f0d25537ffbf02c2b65f97e45c557e
Mason Aviles <mce.aviles@gmail.com>
2021-06-14 15:34:24 -0700

    adding tailwind and the basic page structure for vue and brython

--- game.py
+++ game.py
@@ -1,22 +1,42 @@
 from browser import window
+import javascript
 
 Vue = window.Vue
 
 def data(*args):
-    return 
+    return {
+        'greeting': 'Ready Player Python'
+    }
+
+def handle_click(event):
+    print("clicked!")
+    javascript.this().greeting = "change to this greeting"
+
+def show_names(event):
+    pass
+
+methods = {
+    "handle_click": handle_click,
+    "show_names": show_names
+}
+
+template = """
+    <h2 class="text-4xl">{{ greeting }}</h2>
+    <div>
+        <button @click="handle_click" type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">YO</button>
+    </div>
+"""
 
 
+# Put it all together
 app = Vue.createApp(
     {
-        "template": """
-            <h2>Ready Player Python</h2>
-            <div class="flex flex-col gap-8 w-1/2 mx-auto py-8">
-                <button>YO</button>
-
-            </div>
-        """,
+        "template": template,
+        "data": data,
+        "methods": methods
     }
 )
 
-vm = app.mount("#app")
+# Mount it to #app
+app.mount("#app")
 
