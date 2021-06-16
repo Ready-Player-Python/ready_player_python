4b15d02db4c86886113a9ab538381d94d84dd830
Mason Aviles <mce.aviles@gmail.com>
2021-06-15 18:45:39 -0700

    save progress

--- about_us.py
+++ about_us.py
@@ -1,4 +1,5 @@
-from browser import window, document, html
+from browser import window, document
+from browser import timer
 import javascript
 
 Vue = window.Vue
@@ -21,6 +22,16 @@ def handle_click_show_fighter_mason(event):
     document['body-hunter'].style.display = "none"
     document['body-brian'].style.display = "none"
     document['body-klace'].style.display = "none"
+    document['mason-body'].style.color = "red"
+
+def set_timer_for_typer(event):
+    timer.set_timeout(typewriter, 3000)
+
+def typewriter():
+    txt = 'example text for mason'
+    document['mason-body'].innerHTML = txt
+
+document['type'].bind('click', set_timer_for_typer)
 
 def handle_click_show_fighter_hunter(event):
     document['body-hunter'].style.display = "block"
@@ -46,6 +57,8 @@ methods = {
     "handle_click_show_fighter_hunter": handle_click_show_fighter_hunter,
     "handle_click_show_fighter_brian": handle_click_show_fighter_brian,
     "handle_click_show_fighter_klace": handle_click_show_fighter_klace,
+    "set_timer_for_typer": set_timer_for_typer,
+    "typewriter": typewriter
 }
 
 template = """
@@ -53,7 +66,7 @@ template = """
         <div class="mt-2 w-2/4">
             <h2 class="text-4xl mb-2">{{ greeting }}</h2>
             <div id="panel"></div>
-            <button @click="handle_click_show_fighter_mason" type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white mr-5">{{ fighters[0] }}</button>
+            <button id="type" @click="handle_click_show_fighter_mason" type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white mr-5">{{ fighters[0] }}</button>
 
             <button @click="handle_click_show_fighter_hunter" type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white mr-5">{{ fighters[1] }}</button>
 
@@ -65,7 +78,7 @@ template = """
             <div id="hello">
                 <div id="body-mason" style="display:none">
                     <h2>{{ fighters[0] }}</h2>
-                    <p>body mason</p>
+                    <p id="mason-body">body mason</p>
                 </div>
                 <div id="body-brian" style="display:none">
                     <h2>{{ fighters[3] }}</h2>
