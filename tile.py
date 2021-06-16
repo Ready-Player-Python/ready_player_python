3741849f2897669e6caef02781561b0acc07d8db
Mason Aviles <mce.aviles@gmail.com>
2021-06-16 16:04:04 -0700

    save

--- tile.py
+++ tile.py
@@ -5,87 +5,45 @@ Phaser = window.Phaser
 
 class Game(object):
     def __init__(self):
-        self.game = window.Phaser.Game.new(
-            {
-                'type': Phaser.AUTO,
-                'width': 800,
-                'height': 600,
-                'pixelArt': 'true',
-                'backgroundColor': '#1a1a2d',
-                'scene': {
-                    'preload': self.preload,
-                    'create': self.create
-                }
+        config = {
+            'type': Phaser.WEBGL,
+            'width': 640,
+            'height': 480,
+            'backgroundColor': '#bfcc00',
+            'scene': {
+                'preload': self.preload,
+                'create': self.create,
+                'update': self.update
             }
-        )
+        }
+        self.game = window.Phaser.Game.new(config)
+        self.UP = 0
+        self.DOWN = 0
+        self.LEFT = 0
+        self.RIGHT = 0
 
     def preload(self, *args):
         this = javascript.this()
-        # this.load.setBaseURL('http://labs.phaser.io')
-        this.load.image('tiles', 'http://labs.phaser.io/assets/tilemaps/tiles/drawtiles-spaced.png')
-        this.load.image('car', 'http://labs.phaser.io/assets/sprites/car90.png')
-        this.load.tilemapCSV('map', 'http://labs.phaser.io/assets/tilemaps/csv/grid.csv')
+        this.load.image('food', 'assets/food.png')
+        this.load.image('body', 'assets/body.png')
 
     def create(self, *args):
         this = javascript.this()
 
-        self.map = this.make.tilemap({
-            'key': 'map', 
-            'tileWidth': 32, 
-            'tileHeight': 32 
+        def food(scene, x, y):
+            Phaser.GameObjects.Image.call(this, scene)
+            this.setTexture('food')
+            this.setPosition(x * 16, y * 16)
+            this.setOrigin(0)
+            this.total = 0
+            scene.children.add(this)
+        self.Food = Phaser.Class.new({
+            'Extends': window.Phaser.GameObjects.Image.call(this, this.scene),
+            'initialize': food()
         })
-        self.tileset = map.addTilesetImage('tiles', null, 32, 32, 1, 2)
-        self.layer = map.createLayer(0, tileset, 0, 0)
-        self.player = this.add.image(32+16, 32+16, 'car')
+        self.Snake = Phaser.Class.new({
 
-        # Left
-        def left(event):
-            tile = self.layer.getTileAtWorldXY(self.player.x - 32, self.player.y, true)
-            if tile.index == 2:
-                pass
-            else:
-                self.player.x -= 32
-                self.player.angle = 180
-        this.input.keyboard.on('keydown-A', left)
-
-        # Right
-        def right(event):
-            tile = self.layer.getTileAtWorldXY(self.player.x + 32, self.player.y, true)
-            if tile.index == 2:
-                pass
-            else:
-                self.player.x += 32
-                self.player.angle = 0
-        this.input.keyboard.on('keydown-D', right)
-
-        # Up
-        def up(event):
-            tile = self.layer.getTileAtWorldXY(self.player.x, self.player.y - 32, true)
-            if tile.index == 2:
-                pass
-            else:
-                self.player.x -= 32
-                self.player.angle = -90
-        this.input.keyboard.on('keydown-W', up)
-
-        # Down
-        def down(event):
-            tile = self.layer.getTileAtWorldXY(self.player.x, self.player.y + 32, true)
-            if tile.index == 2:
-                pass
-            else:
-                self.player.x += 32
-                self.player.angle = 90
-
-        # demo code
-        this.add.image(400, 300, 'sky')
-        particles = this.add.particles('red')
-        emitter = particles.createEmitter({
-            'speed': 100,
-            'scale': {'start': 1, 'end': 0},
-            'blendMode': 'NORMAL'
         })
-        this.input.keyboard.on('keydown-S', down)
 
 
 GAME = Game()
