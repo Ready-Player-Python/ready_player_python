c064eb6e68bfa4220630156f34faeed5b28f4d30
Brian Lemons <brian_lemons@hotmail.com>
2021-06-18 11:46:58 -0700

    increasing button size

--- game.py
+++ game.py
@@ -39,8 +39,8 @@ methods = {
 }
 
 template = """
-    <button @click="start_game_slow" class="text-center block bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ring-transparent">Slow Game</button>
-    <button @click="start_game_fast" class="text-center block bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded ring-transparent">Fast Game</button>
+    <button @click="start_game_slow" class="text-center block bg-blue-500 hover:bg-blue-400 text-white font-bold py-3 px-6 border-b-4 border-blue-700 hover:border-blue-500 rounded ring-transparent">Slow Game</button>
+    <button @click="start_game_fast" class="text-center block bg-blue-500 hover:bg-blue-400 text-white font-bold py-3 px-6 border-b-4 border-blue-700 hover:border-blue-500 rounded ring-transparent">Fast Game</button>
     <div class="relative">
         <button @click="handle_display_click" id="instructions-btn" class="btn btn-info text-center block bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-3xl ring-offset-transparent	border-transparent">?</button>
         <div id="help-box" style="display:none" class="absolute w-72 bg-black text-white p-2 -right-20 top-20">
